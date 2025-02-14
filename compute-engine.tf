b8df40aa91d022ff1961e249507c451801927560
mauro meluso <mauro.meluso@macro.com.ar>
2025-02-14 15:54:00 -0300

    first deploy

---
+++ compute-engine.tf
@@ -0,0 +1,29 @@
+module "instance_template" {
+  source  = "terraform-google-modules/vm/google//modules/instance_template"
+  version = "~> 13.0"
+
+  region             = "us-east1"
+  project_id         = "ninth-palace-423513-n5"
+  subnetwork         = "default"
+  subnetwork_project = "ninth-palace-423513-n5"
+  service_account = {
+    email  = "compute-engine@ninth-palace-423513-n5.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+  machine_type	= "e2-micro"
+
+}
+
+module "compute_instance" {
+  source  = "terraform-google-modules/vm/google//modules/compute_instance"
+  version = "~> 13.0"
+
+  region              = "us-east1"
+  zone                = "us-east1-b"
+  subnetwork          = "default"
+  subnetwork_project  = "ninth-palace-423513-n5"
+  num_instances       = "2"
+  hostname            = "instance-simple"
+  instance_template   = module.instance_template.self_link
+  deletion_protection = false
+}
---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "google" {
+  project = "ninth-palace-423513-n5"
+  region  = "us-east1"  
+}
