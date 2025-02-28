ae61b0074919ed0c2d81cc6727212a7bd99c66dd
mauro meluso <mauro.meluso@macro.com.ar>
2025-02-28 16:03:28 -0300

    changes

--- myfirstproject/backend.tf
+++
@@ -1,7 +0,0 @@
-# backend.tf
-terraform {
-  backend "gcs" {
-    bucket = "terraform-states-agnostic"  # Nombre del bucket en GCS
-    prefix = "myfirstproject"           # Prefijo para el archivo de estado (opcional)
-  }
-}
